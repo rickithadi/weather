1cf32fc7298cbfaef974b3b64b4710ddcb19bcb4
rickithadi <rickithadi@gmail.com>
2018-07-31 17:07:18 +0800

    tf

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(cors());
 
 app.use(express.static(__dirname + "/dist"));
 app.listen(process.env.PORT || 8080);
+console.log('running');
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "dist/index.html"));
 });
